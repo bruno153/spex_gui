a230104eb2826aa23def0004f2007b156a76c2fa
Rafael Zuolo <rafael.zuolo.lima@usp.br>
2020-01-31 13:14:40 -0300

    Pastas Novas

---
+++ Hardware/StepMotorControl.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 09:24:28 2020
+
+Controlador dos motores de passo, versão python
+para raspy
+
+@author: rafael
+"""
+
+import gpiozero as io
+from time import sleep
+
+stepPin = io.DigitalOutputDevice(17)
+dirPin = io.DigitalOutputDevice(27)
+stepInterval = .005 # milliseconds
+stopPin1 = io.DigitalInputDevice(22) # positivo
+stopPin2 = io.DigitalInputDevice(23) # negativo
+
+
+
+
+stepPin.off()
+wavelength = Serial.parseInt() # FUNÇÃO QUE VEM DO FRONT END ---------------
+
+def waveToSteps(wave):
+    wave2 = wave
+    rate = 5 # steps/nm
+    return int(rate*wave2)
+
+
+while True():
+    Serial.println("Insert next wavelength:\n") # DEVE SER INTEGRADO NO FRONT----
+    targetwave = Serial.parseInt()
+    Serial.print("Setting wavelength set at ")
+    Serial.print(targetwave/10)
+    Serial.print(".")
+    Serial.print(targetwave%10)
+    Serial.println(" nm.")
+    targetstep = waveToSteps(wavelength-targetwave)
+    Serial.print("This should take ")
+    Serial.print(targetstep)
+    Serial.println(" steps.")
+
+    while targetstep != 0:
+        if targetstep > 0 and stopPin2.value() == 1: # gpio 23
+            dirPin.off()
+            stepPin.on()
+            sleep(stepInterval)
+            targetstep = targetstep - 1
+
+        if targetstep < 0 and stopPin1.value() == 1: # pino 22
+            dirPin.on()
+            stepPin.on()
+            sleep(stepInterval)
+            targetstep = targetstep + 1
+
+        stepPin.off()
+
+    Serial.println("Done!")
+    wavelength = targetwave
---
+++ Hardware/StepMotorControlTESTER.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 09:24:28 2020
+
+Controlador dos motores de passo, versão python
+para raspy
+
+@author: rafael
+"""
+
+import gpiozero as io
+from time import sleep
+
+from gpiozero.pins.mock import MockFactory
+
+io.Device.pin_factory = MockFactory()
+
+stepPin = io.DigitalOutputDevice(17)
+dirPin = io.DigitalOutputDevice(27)
+stepInterval = .5 # milliseconds
+stopPin1 = io.DigitalInputDevice(22) # positivo
+stopPin2 = io.DigitalInputDevice(23) # negativo
+
+stopPin1State = io.Device.pin_factory.pin(22)
+stopPin2State = io.Device.pin_factory.pin(23)
+
+stopPin1State.drive_high()
+stopPin2State.drive_high()
+
+stepPin.off()
+
+
+while True:
+
+    numTestes = int(input("Insert number of tests: "))
+    targetstep = float(input("Insert number of steps "))
+
+    for i in range (0, numTestes):
+        while targetstep != 0:
+            if targetstep > 0 and stopPin2.value == 1: # gpio 23
+                dirPin.off()
+                stepPin.on()
+                sleep(stepInterval)
+                targetstep = targetstep - 1
+
+            if targetstep < 0 and stopPin1.value == 1: # pino 22
+                dirPin.on()
+                stepPin.on()
+                sleep(stepInterval)
+                targetstep = targetstep + 1
+
+            stepPin.off()
+        print("Done!")
+
