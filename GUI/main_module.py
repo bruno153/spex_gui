e29aacf898f924e1c988ac6fb3ff95ea615479fa
Bruno Yamada <bruno153153@gmail.com>
2020-02-03 16:30:30 -0300

    updated main to use window_user

--- GUI/main_module.py
+++ GUI/main_module.py
@@ -3,6 +3,10 @@ import pickle as p
 
 from window_exp_setup import exp_setup
 from window_measure import measure
+from window_login import login
 
+
+user = login()
+print(user)
 values = exp_setup()
 measure(values)
