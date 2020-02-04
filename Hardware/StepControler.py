db2db721d337e235b939168757a0e4c3575b5397
Rafael Zuolo <rafael.zuolo.lima@usp.br>
2020-02-04 09:35:03 -0300

    Create StepControler.py

---
+++ Hardware/StepControler.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 09:24:28 2020
+
+Controlador dos motores de passo, versão python
+para raspy
+
+@author: rafael
+"""
+
+import gpiozero as io
+from time import sleep
+
+#from gpiozero.pins.mock import MockFactory
+#io.Device.pin_factory = MockFactory()
+
+stepPin = io.DigitalOutputDevice(17)
+dirPin = io.DigitalOutputDevice(27)
+stepInterval = .005 # milliseconds
+stopPin1 = io.DigitalInputDevice(22) # positivo
+stopPin2 = io.DigitalInputDevice(23) # negativo
+
+
+
+
+stepPin.off()
+#wavelength = Serial.parseInt() # FUNÇÃO QUE VEM DO FRONT END ---------------
+
+
+def manualStep(targetstep):
+	global stepInterval, dirPin, stepPin, stopPin1, stopPin2
+	while targetstep != 0:	
+		if targetstep > 0 and stopPin2.value() == 1: # gpio 23
+			dirPin.off()
+			stepPin.on()
+			sleep(stepInterval)
+			targetstep = targetstep - 1
+		if targetstep < 0 and stopPin1.value() == 1: # pino 22
+			dirPin.on()
+			stepPin.on()
+			sleep(stepInterval)
+			targetstep = targetstep + 1
+		stepPin.off()
+
+
+def waveToSteps(wave):
+    wave2 = wave	
+    rate = 5 # steps/nm
+    return int(rate*wave2)
+
+
+# while True():
+    # Serial.println("Insert next wavelength:\n") # DEVE SER INTEGRADO NO FRONT----
+    # targetwave = Serial.parseInt()
+    # Serial.print("Setting wavelength set at ")
+    # Serial.print(targetwave/10)
+    # Serial.print(".")
+    # Serial.print(targetwave%10)
+    # Serial.println(" nm.")
+    # targetstep = waveToSteps(wavelength-targetwave)
+    # Serial.print("This should take ")
+    # Serial.print(targetstep)
+    # Serial.println(" steps.")
+
+    # while targetstep != 0:
+        # if targetstep > 0 and stopPin2.value() == 1: # gpio 23
+            # dirPin.off()
+            # stepPin.on()
+            # sleep(stepInterval)
+            # targetstep = targetstep - 1
+
+        # if targetstep < 0 and stopPin1.value() == 1: # pino 22
+            # dirPin.on()
+            # stepPin.on()
+            # sleep(stepInterval)
+            # targetstep = targetstep + 1
+
+        # stepPin.off()
+
+    # Serial.println("Done!")
+    # wavelength = targetwave
