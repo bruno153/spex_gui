dc82866145496906953c0e779977d6e9307a19ed
Rafael Zuolo <rafael.zuolo.lima@usp.br>
2020-02-04 14:52:54 -0300

    edit window_manual_control to accept StepControler

--- GUI/window_manual_control.py
+++ GUI/window_manual_control.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
-
+import sys
+sys.path.append('../')
 import PySimpleGUI as sg
 from math import sin
+from Hardware.StepControler import wave_step
 
 sg.ChangeLookAndFeel('DarkBlue')
 
-def manual_control():
-	exVal = 4000
-	emVal = 3000
+def manual_control(pin_list_ex, pin_list_em):
+	ex_val = 4000
+	em_val = 3000
 
 	sg.SetOptions(text_justification='left')
 	layout_manualControl = [
-	   [sg.Text('Manual Control', font=('Helvetica', 16))],
-	   [sg.Radio('Excitation', 'control', default=True, key='ex'), sg.Radio('Emission', 'control', key='em')],
-	   [sg.Button('-100 nm', size=(8, 1), key='-100'), sg.Button('-10 nm', size=(7, 1), key='-10'), sg.Button('-1 nm', size=(7, 1), key='-1'), sg.Button('-0.1 nm', size=(7, 1), key='-0.1')],
-	   [sg.Button('+100 nm', size=(8, 1), key='+100'), sg.Button('+10 nm', size=(7, 1), key='+10'), sg.Button('+1 nm', size=(7, 1), key='+1'), sg.Button('+0.1 nm', size=(7, 1), key='+0.1')],
-	   [sg.Text('Current excitation: ', size=(15, 1)), sg.Text(str(exVal/10), size=(4, 1), key='exVal')],
-	   [sg.Text('Current emition: ', size=(15, 1)), sg.Text(str(emVal/10), size=(4, 1), key='emVal')],
-	   [sg.Text('Measure: ', size=(15, 1)), sg.Text('0', size=(4,1), justification='right', key='measure')],
-	   [sg.Graph(canvas_size=(270, 270), graph_bottom_left=(0,0), graph_top_right=(200, 200), background_color='white', key='graph')],
-	   [sg.Quit()]
+		[sg.Text('Manual Control', font=('Helvetica', 16))],
+		[sg.Radio('Excitation', 'control', default=True, key='ex'), sg.Radio('Emission', 'control', key='em')],
+		[sg.Button('-100 nm', size=(8, 1), key='-100'), sg.Button('-10 nm', size=(7, 1), key='-10'),
+		sg.Button('-1 nm', size=(7, 1), key='-1'), sg.Button('-0.1 nm', size=(7, 1), key='-0.1')],
+		[sg.Button('+100 nm', size=(8, 1), key='+100'), sg.Button('+10 nm', size=(7, 1), key='+10'), 
+		sg.Button('+1 nm', size=(7, 1), key='+1'), sg.Button('+0.1 nm', size=(7, 1), key='+0.1')],
+		[sg.Text('Current excitation: ', size=(13, 1)), sg.Text(str(ex_val/10), size=(5, 1), key='ex_val'), 
+		sg.Button('Set value', size=(7,1), key='set_ex_btn'), sg.Input(size=(5,1), key='set_ex_val')],
+		[sg.Text('Current emition: ', size=(13, 1)), sg.Text(str(em_val/10), size=(5, 1), key='em_val'),
+		sg.Button('Set value', size=(7,1), key='set_em_btn'), sg.Input(size=(5,1), key='set_em_val')],
+		[sg.Text('Measure: ', size=(15, 1)), sg.Text('0', size=(4,1), justification='right', key='measure')],
+		[sg.Graph(canvas_size=(270, 270), graph_bottom_left=(0,0), graph_top_right=(200, 200), background_color='white', key='graph')],
+		[sg.Quit()]
 
 	]
 	x=0
@@ -30,27 +36,40 @@ def manual_control():
 	graph = window_manualControl['graph']
 
 	while True:
-		 manCtrl_event, values = window_manualControl.read(timeout=10)
-		 if manCtrl_event in (None, 'Quit'):
-			  break
+		manCtrl_event, values = window_manualControl.read(timeout=10)
+		if manCtrl_event in (None, 'Quit'):
+			break
 
-		 elif manCtrl_event in ('-100', '-10', '-1', '-0.1', '+100', '+10', '+1', '+0.1'):
-			  val = int(float(manCtrl_event)*10)
-			  if values['ex']:
-				   exVal = exVal + val
-				   window_manualControl['exVal'].Update(exVal/10)
-			  else:
-				   emVal = emVal + val
-				   window_manualControl['emVal'].Update(emVal/10)
+		if manCtrl_event=='set_ex_btn':
+			ex_val = int(float(values['set_ex_val'])*10)
+			window_manualControl['ex_val'].Update(ex_val/10)
+			
+		if manCtrl_event=='set_em_btn':
+			em_val = int(float(values['set_em_val'])*10)
+			window_manualControl['em_val'].Update(em_val/10)
 
-		 graph.Erase()
-		 if i >= 200:
-			  i = 0
-		 points[i] = (i, 100*sin(3.1415629*1310*x) + 100)
-		 for j in range (0, 200):
-			  graph.DrawPoint((points[j]), 1, color='green')
-		 window_manualControl['measure'].Update('{0:.1f}'.format(100*sin(3.1415629*1310*x) + 100))
-		 x = x + 1
-		 i = i + 1
+		if manCtrl_event in ('-100', '-10', '-1', '-0.1', '+100', '+10', '+1', '+0.1'):
+			val = int(float(manCtrl_event)*10)
+			if values['ex']:
+				wave_step(val, pin_list_ex)
+				ex_val = ex_val + val
+				window_manualControl['ex_val'].Update(ex_val/10)
+			else:
+				wave_step(val, pin_list_em)
+				em_val = em_val + val
+				window_manualControl['em_val'].Update(em_val/10)
+
+		graph.Erase()
+		if i >= 200:
+			i = 0
+		points[i] = (i, 100*sin(3.1415629*1310*x) + 100)
+		for j in range (0, 200):
+			graph.DrawPoint((points[j]), 1, color='green')
+		window_manualControl['measure'].Update('{0:.1f}'.format(100*sin(3.1415629*1310*x) + 100))
+		x = x + 1
+		i = i + 1
 
 	window_manualControl.close()
+	
+# PARA RODAR A JANELA, BASTA DESCOMENTAR E RODAR window_manual_Control.py
+#manual_control({},{})
--- Hardware/StepControler.py
+++ Hardware/StepControler.py
@@ -52,35 +52,6 @@ def waveToSteps(wave):
     wave2 = wave	
     rate = 5 # steps/nm
     return int(rate*wave2)
-
-
-# while True():
-    # Serial.println("Insert next wavelength:\n") # DEVE SER INTEGRADO NO FRONT----
-    # targetwave = Serial.parseInt()
-    # Serial.print("Setting wavelength set at ")
-    # Serial.print(targetwave/10)
-    # Serial.print(".")
-    # Serial.print(targetwave%10)
-    # Serial.println(" nm.")
-    # targetstep = waveToSteps(wavelength-targetwave)
-    # Serial.print("This should take ")
-    # Serial.print(targetstep)
-    # Serial.println(" steps.")
-
-    # while targetstep != 0:
-        # if targetstep > 0 and stopPin2.value() == 1: # gpio 23
-            # dirPin.off()
-            # stepPin.on()
-            # sleep(stepInterval)
-            # targetstep = targetstep - 1
-
-        # if targetstep < 0 and stopPin1.value() == 1: # pino 22
-            # dirPin.on()
-            # stepPin.on()
-            # sleep(stepInterval)
-            # targetstep = targetstep + 1
-
-        # stepPin.off()
-
-    # Serial.println("Done!")
-    # wavelength = targetwave
+	
+def wave_step(targetwave, pin_list):
+	manual_step(waveToSteps(targetwave), pin_list)
