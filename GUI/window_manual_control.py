545ba15aef8d20f92212e3fad6d583be357d99c1
Rafael Zuolo <rafael.zuolo.lima@usp.br>
2020-02-12 14:28:54 -0300

    tried to add de ADS to window_manual_control

--- GUI/window_manual_control.py
+++ GUI/window_manual_control.py
@@ -4,6 +4,28 @@ sys.path.append('../')
 import PySimpleGUI as sg
 from math import sin
 from Hardware.DeclarativeStepControler import wave_step
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.ads1x15 import Mode
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Data collection setup
+RATE = 475
+
+# Create the I2C bus
+# Set frequency high to reduce time spent with I2C comms
+i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1115(i2c)
+
+# Create single-ended input on channel 0
+chan0 = AnalogIn(ads, ADS.P0)
+
+# ADC Configuration
+ads.mode = Mode.CONTINUOUS
+ads.data_rate = RATE
 
 sg.ChangeLookAndFeel('DarkBlue')
 
@@ -17,27 +39,32 @@ def manual_control(pin_list_ex, pin_list_em):
     layout_manualControl = [
         [sg.Text('Manual Control', font=('Helvetica', 16))],
         [sg.Radio('Excitation', 'control', default=True, key='ex'), sg.Radio('Emission', 'control', key='em')],
-        [sg.Button('', size=(8, 1), key='-100', image_filename='assets/buttons/-100.png', image_size=(80,80), border_width=0), 
-        	sg.Button('', size=(7, 1), key='-10', image_filename='assets/buttons/-10.png', image_size=(80,80), border_width=0),
-            sg.Button('', size=(7, 1), key='-1', image_filename='assets/buttons/-1.png', image_size=(80,80), border_width=0), 
-            sg.Button('', size=(7, 1), key='-0.1', image_filename='assets/buttons/-0.1.png', image_size=(80,80), border_width=0),
-            sg.Button('', size=(7, 1), key='+0.1', image_filename='assets/buttons/+0.1.png', image_size=(80,80), border_width=0),
-            sg.Button('', size=(7, 1), key='+1', image_filename='assets/buttons/+1.png', image_size=(80,80), border_width=0), 
-            sg.Button('', size=(8, 1), key='+10', image_filename='assets/buttons/+10.png', image_size=(80,80), border_width=0), 
-        	sg.Button('', size=(7, 1), key='+100', image_filename='assets/buttons/+100.png', image_size=(80,80), border_width=0)
+        [sg.Button('', size=(8, 1), key='-100', image_filename='assets/buttons/-100.png', image_size=(50,50), border_width=0), 
+        	sg.Button('', size=(7, 1), key='-10', image_filename='assets/buttons/-10.png', image_size=(50,50), border_width=0),
+            sg.Button('', size=(7, 1), key='-1', image_filename='assets/buttons/-1.png', image_size=(50,50), border_width=0), 
+            sg.Button('', size=(7, 1), key='-0.1', image_filename='assets/buttons/-0.1.png', image_size=(50,50), border_width=0),
+            sg.Button('', size=(7, 1), key='+0.1', image_filename='assets/buttons/+0.1.png', image_size=(50,50), border_width=0),
+            sg.Button('', size=(7, 1), key='+1', image_filename='assets/buttons/+1.png', image_size=(50,50), border_width=0), 
+            sg.Button('', size=(8, 1), key='+10', image_filename='assets/buttons/+10.png', image_size=(50,50), border_width=0), 
+        	sg.Button('', size=(7, 1), key='+100', image_filename='assets/buttons/+100.png', image_size=(50,50), border_width=0)
         ],
-        [sg.Text('Current excitation: ', size=(13, 1)), sg.Text(str(ex_val/10), size=(5, 1), key='ex_val'), 
-            sg.Button('Set value', size=(7,1), key='set_ex_btn'), sg.Input(size=(5,1), key='set_ex_val')],
-        [sg.Text('Current emition: ', size=(13, 1)), sg.Text(str(em_val/10), size=(5, 1), key='em_val'),
-            sg.Button('Set value', size=(7,1), key='set_em_btn'), sg.Input(size=(5,1), key='set_em_val')],
-        [sg.Text('Measure: ', size=(15, 1)), sg.Text('0', size=(4,1), justification='right', key='measure')],
-        [sg.Graph(canvas_size=(270, 270), graph_bottom_left=(0,0), graph_top_right=(200, 200), background_color='white', key='graph')],
+        [sg.Button('Set value', size=(10,1), key='set_btn'), 
+            sg.Button('Go to value', size=(10,1), key='goto_btn'), 
+            sg.Input(size=(6,1), key='set_goto_val')],
+        [sg.Text('Current excitation: ', size=(22, 1)), 
+            sg.Text(str(ex_val/10), justification='right', size=(5, 1), key='ex_val')],
+        [sg.Text('Current emition: ', size=(22, 1)), 
+            sg.Text(str(em_val/10), justification='right', size=(5, 1), key='em_val')],
+        [sg.Text('Measure: ', size=(22, 1)), 
+            sg.Text('0', size=(5,1), justification='right', key='measure')],
+        [sg.Graph(canvas_size=(270, 270), graph_bottom_left=(0,0), 
+                  graph_top_right=(200, 200), background_color='white', key='graph')],
         [sg.Quit(), sg.Submit()]
 
     ]
     x=0
     i=0
-    points = [(i, 100) for i in range (0, 200)]
+    points = [(i, 10) for i in range (0, 3*RATE)]
     window_manualControl = sg.Window('SPEX control', layout_manualControl)
     window_manualControl.Finalize()
     graph = window_manualControl['graph']
@@ -53,22 +80,36 @@ def manual_control(pin_list_ex, pin_list_em):
         if manCtrl_event=='Submit':
             break
         
-        #Set buttons handler
-        if manCtrl_event=='set_ex_btn':
+        #Set and goto buttons handler
+        if manCtrl_event=='set_btn':
             try:
-                ex_val = int(float(values['set_ex_val'])*10)
-                window_manualControl['ex_val'].Update(ex_val/10)
-                window_manualControl['set_ex_val'].Update('')
+                temp_val = int(float(values['set_val'])*10)
+                if values['ex']:
+                    ex_val = temp_val
+                    window_manualControl['ex_val'].Update(ex_val/10)
+                else:
+                    em_val = temp_val
+                    window_manualControl['em_val'].Update(em_val/10)
             except:
                 sg.PopupOK('Escreva um número (use ponto para o decimal)', title='Erro')
-        if manCtrl_event=='set_em_btn':
-            try:    
-                em_val = int(float(values['set_em_val'])*10)
-                window_manualControl['em_val'].Update(em_val/10)
-                window_manualControl['set_em_val'].Update('')
+            window_manualControl['set_goto_val'].Update('')
+            
+        if manCtrl_event=='goto_btn':
+            try:
+                temp_val = int(float(values['set_val'])*10)
+                if values['ex'] is True:
+                    delta_val = temp_val - ex_val
+                    ex_val = temp_val
+                    window_manualControl['ex_val'].Update(ex_val/10)
+                    wave_step(delta_val/10, pin_list_ex)
+                else:
+                    delta_val = temp_val - em_val
+                    em_val = temp_val
+                    window_manualControl['em_val'].Update(em_val/10)
+                    wave_step(delta_val/10, pin_list_em)
             except:
                 sg.PopupOK('Escreva um número (use ponto para o decimal)', title='Erro')
-        
+            window_manualControl['set_goto_val'].Update('')
         #Step Motor control handler
         if manCtrl_event in ('-100', '-10', '-1', '-0.1', '+100', '+10', '+1', '+0.1'):
             val = int(float(manCtrl_event)*10)
@@ -85,7 +126,7 @@ def manual_control(pin_list_ex, pin_list_em):
         graph.Erase()
         if i >= 200:
             i = 0
-        points[i] = (i, 100*sin(3.1415629*1310*x) + 100)
+        points[i] = (i, chan.value/260)
         for j in range (0, 200):
             graph.DrawPoint((points[j]), 1, color='green')
         window_manualControl['measure'].Update('{0:.1f}'.format(100*sin(3.1415629*1310*x) + 100))
